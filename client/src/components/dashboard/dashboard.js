fb9ce8a55d6a01c70618b287bbab04fdbddcf9b7
zoro <zoro@pop-os.localdomain>
2022-02-21 17:31:07 +0530

    all competed

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,47 +1,80 @@
 import axios from 'axios'
 import { useEffect,useState } from 'react';
-
+import './dashboard.css'
+import Cookies from 'js-cookie'
 const Dashboard = ()=>{
     const [username,setUsername] = useState("")
     const [pictureSrc,setPictureSrc] = useState("")
     const [email,setEmail] = useState("")
     useEffect(()=>{
-        const urlParams = new URLSearchParams(window.location.search)
-        const socialMediaName = urlParams.get('state')
-        if(socialMediaName==='google'){
-            const authorizationCode = urlParams.get('code')
-            const object = {
-                authorizationCode
-            }
-            axios.post(window.server_url+'/authcode/google',object)
-            .then(response=>{
-                setUsername(response.data.name)
-                setPictureSrc(response.data.picture)
-                setEmail(response.data.email)
-            })
-            .catch(err=>console.error(err))
+        //if cookie is already set load that 
+        if(Cookies.get('username')!==undefined){
+            setUsername(Cookies.get('username'))
+            setPictureSrc(Cookies.get('pictureSrc'))
+            setEmail(Cookies.get('email'))
         }
-        else if(socialMediaName==='facebook'){
-            const authorizationCode = urlParams.get('code')
-            const object = {
-                authorizationCode
+        else{
+            const urlParams = new URLSearchParams(window.location.search)
+            const socialMediaName = urlParams.get('state')
+            if(socialMediaName==='google'){
+                const authorizationCode = urlParams.get('code')
+                const object = {
+                    authorizationCode
+                }
+                axios.post(window.server_url+'/authcode/google',object)
+                .then(response=>{
+                    setUsername(response.data.name)
+                    setPictureSrc(response.data.picture)
+                    setEmail(response.data.email)
+                    //saving user data to cookies
+                    document.cookie=`username=${response.data.name}`
+                    document.cookie=`email=${response.data.email}`
+                    document.cookie=`pictureSrc=${response.data.picture}`
+                })
+                .catch(err=>console.error(err))
             }
-            axios.post(window.server_url+'/authcode/facebook',object)
-            .then(response=>{
-                setUsername(response.data.name)
-                setPictureSrc(response.data.picture.data.url)
-                setEmail(response.data.email)
-
-            })
-            .catch(err=>console.error(err))
+            else if(socialMediaName==='facebook'){
+                const authorizationCode = urlParams.get('code')
+                const object = {
+                    authorizationCode
+                }
+                axios.post(window.server_url+'/authcode/facebook',object)
+                .then(response=>{
+                    setUsername(response.data.name)
+                    setPictureSrc(response.data.picture.data.url)
+                    setEmail(response.data.email)
+                    //saving user data to cookies
+                    document.cookie=`username=${response.data.name}`
+                    document.cookie=`email=${response.data.email}`
+                    document.cookie=`pictureSrc=${response.data.picture.data.url}`
+    
+                })
+                .catch(err=>console.error(err))
+            }
         }
         
+        
+        
+        
     },[])
+    function logOut(){
+        Cookies.remove('username',{path:''})
+        Cookies.remove('email',{path:''})
+        Cookies.remove('pictureSrc',{path:''})
+
+        window.open('http://localhost:3000/','_self')
+    }
     return(
         <section id='logged_in_container'>
-            <h1>welcome {username}</h1>
-            <img src={pictureSrc}></img>
-            <h2>your mail {email}</h2>
+            <button onClick={()=>logOut()} className='button is-medium is-warning m-3' id="logout">Log Out</button>
+            <div id='info_container'>
+                <div>
+                <h1 className='is-size-3'>Welcome {username}</h1>
+                <img src={pictureSrc}></img>
+                <h4 className='is-size-4'>Your mail is : {email}</h4>
+                </div>
+                
+            </div>
         </section>
     )
 }
--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -2,8 +2,10 @@ import './home.css'
 import {BsGoogle} from 'react-icons/bs'
 import axios from 'axios'
 import {FiFacebook} from 'react-icons/fi'
+import Cookies from 'js-cookie'
 
 const Home = ()=>{
+  
     function login(social_media){
       if(social_media==='google'){
         axios.get(window.server_url+'/consentWindow/google')
@@ -21,10 +23,14 @@ const Home = ()=>{
       }
   
     }
+    //if cookie is set then redirect to dashboard
+    if(Cookies.get('username')!==undefined){
+      window.location = 'http://localhost:3000/dashboard'
+    }
     return (
         <div className="home">
           <header className='main_title'>
-            <h1 className='is-size-2 has-text-centered'>Welcome to cucumber App :)</h1>
+            <h1 className='is-size-2 has-text-centered'>Welcome to Social login Demo App :)</h1>
             
           </header>
           <section className='mt-6'>
@@ -34,13 +40,13 @@ const Home = ()=>{
                 <span className="icon">
                   <BsGoogle/>
                 </span>
-                <span className=''>Continue With Google</span>
+                <span className='is-size-6'>Continue With Google</span>
               </button>
               <button onClick={()=>login('facebook')} className="login_method_btn">
                 <span className="icon">
                   <FiFacebook size="20px"/>
                 </span>
-                <span className=''>Continue With Facebook</span>
+                <span className='is-size-6'>Continue With Facebook</span>
               </button>
             </div>
           </section>
